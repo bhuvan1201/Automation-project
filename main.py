aff5c187ab6417d2b4b835602a9230dbc17330f6
bhuvan1201 <bhuvang100@gmail.com>
2024-05-12 22:42:55 +0530

    both sheet and code

--- main.py
+++ main.py
@@ -24,21 +24,23 @@ def send_reminder_email(receiver_email, subject, message):
         smtp.send_message(msg)
 
 # Load Excel file
-wb = openpyxl.load_workbook(r'C:\Users\bhuva\OneDrive\Desktop\Python course\Automation\Daily monitering tasks - HR and Corps.xlsx')
+wb = openpyxl.load_workbook(r'C:\Users\bhuva\OneDrive\Desktop\Python course\Automation-project\May-2024- Windows Server Patching-updated.xlsx')
 sheet = wb.active
 
 # Group servers by date
 servers_by_date = defaultdict(list)
 
 for row in sheet.iter_rows(min_row=2, values_only=True):
-    if len(row) >= 3:
-        server_name, application, date_str, *_ = row
-        date_obj = datetime.strptime(str(date_str), '%Y-%m-%d %H:%M:%S')
+    if len(row) >= 4:
+        server_name, maintenance_window, date_str, app_team_mail_id = row
+        if isinstance(date_str, datetime):
+            date_str = date_str.strftime('%d-%m-%Y')
+        date_obj = datetime.strptime((date_str), '%d-%m-%Y')
         reminder_date = date_obj - timedelta(days=1)
         if reminder_date.date() == datetime.now().date():
-            subject = f'Reminder: Patching for {server_name} tomorrow'
-            message = f"Hi,\n\nThis is a reminder that the server {server_name} with the application {application} is scheduled for patching tomorrow, on {date_str}.\n\nRegards,\nYour Team"
-            send_reminder_email('bhuvanm0806@gmail.com', subject, message)
+            subject = f'Reminder: Windows patching for {server_name} of {app_team_mail_id} tomorrow'
+            message = f"Hi,\n\nThis is a reminder about the maintenance window for the server {server_name} scheduled for tomorrow, {date_str}. The maintenance window is from {maintenance_window}.\n\nPlease ensure that the necessary actions are taken.\n\nRegards,\nYour Team"
+            send_reminder_email("Bhuvan.Gowda@staples.com", subject, message)
     else:
-        print(f"Skipping row: {row}. Expected at least 3 values.")
+        print(f"Skipping row: {row}. Expected at least 4 values.")
 print("Reminder emails sent successfully.")
